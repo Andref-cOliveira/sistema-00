318a2537f9320c2df7b115ccbe9259c530aa7435
André Oliveira <afelip.co@gmail.com>
2017-02-21 17:20:23 -0300

    Configuração do HXPHP Framework

--- app/config.php
+++ app/config.php
@@ -3,6 +3,15 @@
 	$configs = new HXPHP\System\Configs\Config;
 
 	$configs->env->add('development');
+	$configs->env->development->baseURI = '/sistema/';
+	$configs->env->development->database->setConnectionData(array(
+			'driver' => 'mysql',
+			'host' => 'localhost',
+			'user' => 'root',
+			'password' => '',
+			'dbname' => 'sistema01',
+			'charset' => 'utf8'
+		));
 
 	/*
 		//Globais
